9ec4c7eb5564e772d3f7ba35eeacb3247774d30c
takira <ca.fie.0220@gmail.com>
2025-01-14 18:03:51 +0900

    add: page-table

--- kernel.h
+++ kernel.h
@@ -41,15 +41,22 @@ struct trap_frame {
 } __attribute__((packed));
 
 struct process {
-    int     pid;          // プロセスID
-    int     state;        // プロセスの状態 PROC_UNUSED or PROC_RUNNABLE
-    vaddr_t sp;           // コンテキストスイッチ時のスタックポインタ
-    uint8_t stack[8192];  // カーネルスタック
+    int         pid;          // プロセスID
+    int         state;        // プロセスの状態 PROC_UNUSED or PROC_RUNNABLE
+    vaddr_t     sp;           // コンテキストスイッチ時のスタックポインタ
+    uint32_t    *page_table;  // 1段目のページテーブルを指すポインタ
+    uint8_t     stack[8192];  // カーネルスタック
 };
 
 # define PROCS_MAX      8  // 最大プロセス数
 # define PROC_UNUSED    0  // 未使用のプロセス管理構造体
 # define PROC_RUNNABLE  1  // 実行可能なプロセス
+# define SATP_SV32      (1u << 31)
+# define PAGE_V         (1 << 0)   // 有効化ビット
+# define PAGE_R         (1 << 1)   // 読み込み可能
+# define PAGE_W         (1 << 2)   // 書き込み可能
+# define PAGE_X         (1 << 3)   // 実行可能
+# define PAGE_U         (1 << 4)   // ユーザーモードでアクセス可能
 
 # define PANIC(fmt, ...)                                                        \
     do {                                                                        \
